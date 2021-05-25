a72fca5b1ebd1a4dd9024e35f28f4959d0164345
Formacion <formacion@FTAULAS.ORG>
2021-05-25 10:18:16 +0200

    number

---
+++ 4.1crear numeros/main.js
@@ -0,0 +1,11 @@
+const numeroEntero = 28;
+console.log(numeroEntero);
+
+const numeroFloat = 28.20;
+console.log(numeroFloat);
+
+const numeroNegativo = -1;
+console.log(numeroNegativo);
+
+const noSoyNumero = "30";
+console.log( typeof noSoyNumero);
---
+++ 4.2operaciones/main.js
@@ -0,0 +1,19 @@
+const numero1 =30;
+const numero2 = 50;
+
+let resultado; 
+//suma:
+resultado = numero1 + numero2;
+console.log(resultado);
+
+resultado = numero1 - numero2;
+console.log(`el resultado de la resta es ${resultado}`);
+//multiplicar:
+resultado = numero1 * numero2;
+console.log(resultado);
+//dividir:
+resultado = numero1 / numero2;
+console.log(resultado);
+//modulo, que es el restante
+resultado= numero1 % numero2;
+console.log(resultado);
